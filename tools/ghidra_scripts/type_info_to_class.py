19da4cc6b9213b05088d664a96baa5c1c6a88e11
Seph De Busser <seph.debusser@gmail.com>
2026-08-21 19:54:07 +0200

    One more function belonging to GlobalManager marked, updated type_info script to pyghidra

--- tools/ghidra_scripts/type_info_to_class.py
+++ tools/ghidra_scripts/type_info_to_class.py
@@ -3,23 +3,26 @@
 #@category GameCube/Wii
 #@keybinding 
 #@menupath 
-#@toolbar 
-#@runtime Jython
+#@toolbar
 
+import typing
+
+if typing.TYPE_CHECKING:
+    from ghidra.ghidra_builtins import *
 from ghidra.app.util import NamespaceUtils
 from ghidra.program.model.symbol import SourceType
 
-def getAllDefinedData():
+def get_all_defined_data():
    data = getFirstData()
    while data:
       yield data
       data = getDataAfter(data)
 
-def getDefinedStructures(structure_name):
-    return [data for data in getAllDefinedData() if data.isStructure() and data.getDataType().getName() == structure_name]
+def get_defined_structures(structure_name):
+    return [data for data in get_all_defined_data() if data.isStructure() and data.getDataType().getName() == structure_name]
 
 
-for a in getDefinedStructures('type_info_struct'):
+for a in get_defined_structures('type_info_struct'):
    sym = getSymbolAt(a.getAddress())
    if sym is None:
      sym = createLabel(a.getAddress(),"type_info_struct",True,SourceType.ANALYSIS)
@@ -27,4 +30,5 @@ for a in getDefinedStructures('type_info_struct'):
      namespace = NamespaceUtils.createNamespaceHierarchy(getDataAt(a.getComponentAt(0).getValue()).getValue().replace(' ',''),None,currentProgram,SourceType.ANALYSIS)
      namespace = NamespaceUtils.convertNamespaceToClass(namespace)
      sym.doSetNameAndNamespace('__RTTI',namespace,SourceType.ANALYSIS,True)
+
 
