cb44d9b8ed514614a013c057332244eefa7c6069
Seph De Busser <seph.debusser@gmail.com>
2026-08-23 22:30:05 +0200

    ObjectManager and SceneImpl progress

--- tools/ghidra_scripts/type_info_to_class.py
+++ tools/ghidra_scripts/type_info_to_class.py
@@ -7,10 +7,31 @@
 
 import typing
 
+from ghidra.program.model.address import Address
+
 if typing.TYPE_CHECKING:
     from ghidra.ghidra_builtins import *
 from ghidra.app.util import NamespaceUtils
+from ghidra.program.model.listing import Data
 from ghidra.program.model.symbol import SourceType
+
+class TypeInfo:
+    def __init__(self, data:Data):
+        self.data = data
+
+    def typename_address(self) -> Address:
+        return typing.cast(Address,self.data.getComponent(0).getValue())
+
+    def typename(self):
+        return getDataAt(self.typename_address()).getValue()
+
+    def has_bases(self):
+        return typing.cast(Address,self.data.getComponent(1).getValue()) != toAddr(0)
+
+    def namespace(self):
+        return NamespaceUtils.createNamespaceHierarchy(self.typename().replace(' ',''),None,currentProgram,SourceType.ANALYSIS)
+
+
 
 def get_all_defined_data():
    data = getFirstData()
